bc17930f174f420380fab79a0346524a8875f0ec
karankumarshreds <naileditnerd@gmail.com>
2021-03-31 20:25:20 +0530

    update function

--- auction-service/src/handlers/process-auctions.js
+++ auction-service/src/handlers/process-auctions.js
@@ -1,23 +1,27 @@
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 import wrapper from '../lib/wrapper';
-import createError from 'http-errors';
-import { getAuctionById } from '../handlers/get-auction';
+// import createError from 'http-errors';
+// import { getAuctionById } from '../handlers/get-auction';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const processAuctions = async (event, context) => {
-  const id = event.pathParameters;
-  const auction = getAuctionById(id);
-  try {
-    // documentdb query
-  } catch (error) {
-    console.error(error);
-    throw new createError.InternalServerError(error);
-  }
-  return {
-    statusCode: 201,
-    body: 'All auctions processed',
-  };
+  // const id = event.pathParameters;
+  // const auction = getAuctionById(id);
+  // let count = 0;
+
+  // try {
+  //   // documentdb query
+
+  // } catch (error) {
+  //   console.error(error);
+  //   throw new createError.InternalServerError(error);
+  // }
+  // return {
+  //   statusCode: 201,
+  //   body: 'All auctions processed',
+  // };
+  console.log('Processing Auctions');
 };
 
 export const handler = wrapper(processAuctions);
